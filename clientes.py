3d1bcccb677362267413ee3bd9f58367744ee874
FerchoJpt-Controlc <jesustarax6@gmail.com>
2025-09-01 10:41:55 -0600

    guardar los datos de clientes

---
+++ clientes.py
@@ -0,0 +1,37 @@
+from GuardarDatosEnArchivos import GestorArchivos
+
+class Cliente:
+    def __init__(self, IDCliente, Nombre, Direccion, Telefono, Correo):
+        self.IDCliente = IDCliente
+        self.Nombre = Nombre
+        self.Direccion = Direccion
+        self.Telefono = Telefono
+        self.Correo = Correo
+
+
+class GestorClientes:
+    def __init__(self):
+        self.archivo = GestorArchivos("data/CLIENTES.txt")
+        self.clientes: dict[str, Cliente] = {}
+        self.cargar_clientes()
+
+    def cargar_clientes(self):
+        lineas = self.archivo.cargar()
+        for linea in lineas:
+            IDCliente, Nombre, Direccion, Telefono, Correo = linea.strip().split(",")
+            self.clientes[IDCliente] = Cliente(IDCliente, Nombre, Direccion, Telefono, Correo)
+
+    def guardar_clientes(self):
+        datos = []
+        for cliente in self.clientes.values():
+            datos.append(f"{cliente.IDCliente},{cliente.Nombre},{cliente.Direccion},{cliente.Telefono},{cliente.Correo}")
+        self.archivo.guardar(datos)
+
+    def agregar_cliente(self, cliente: Cliente):
+        self.clientes[cliente.IDCliente] = cliente
+        self.guardar_clientes()
+
+    def mostrar_clientes(self):
+        print("\n--- LISTA DE CLIENTES ---")
+        for c in self.clientes.values():
+            print(f"{c.IDCliente} | {c.Nombre} | {c.Direccion} | {c.Telefono} | {c.Correo}")
